f1c824ac01b6d2cee03ded03704b8d29088b81d1
unknown <OO345@ccs.local>
2023-10-20 01:00:01 +0800

    this is my first commit

---
+++ PRELIM/FavoriteSong.java
@@ -0,0 +1,9 @@
+public class FavoriteSong{
+   public static void main(String[]args){
+   //Careless Whisper - George Michael
+       System.out.println("I feel so unsure");
+       System.out.println("As I take your hand and lead you to the dance floor");
+       System.out.println("As the music dies, something in your eyes");
+       System.out.println("Calls to mind the silver screen and all its sad goodbyes");
+   }
+}
